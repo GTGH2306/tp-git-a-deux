b4edc408811d56e49b1eacacfad0e048a80476e6
AnthonyMAURICE <116059905+AnthonyMAURICE@users.noreply.github.com>
2024-01-23 14:50:28 +0100

    Update : etape 2

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Person personTest = new Person("Test", "Retest");
+
+Console.WriteLine(personTest.lastName + " " + personTest.firstName);
+
+public class Person{
+    public string lastName;
+    public string firstName;
+    public Person(string _lastName, string _firstName){
+        this.lastName = _lastName;
+        this.firstName = _firstName;
+    }
+    
+}
