8c8a618c487e0c7e8ffc68ed4a54fcc5c6e7e760
TGrandgirard PC-Cours <137295165+GTGH2306@users.noreply.github.com>
2024-01-23 16:15:04 +0100

    Ajout PeopleContainer et SortByLastName/FirstName

--- src/Program.cs
+++ src/Program.cs
@@ -13,6 +13,15 @@ Person myPerson = new(lastName, firstName);
 
 Console.WriteLine(myPerson.lastName + " " + myPerson.firstName);
 
+List<Person> people = new List<Person>();
+
+people.Add(new Person("Watson", "Bobby"));
+people.Add(new Person("Lincoln", "Abraham"));
+people.Add(new Person("Prime", "Optimus"));
+people.Add(new Person("Zord", "Mega"));
+
+
+
 public class Person{
     public string lastName;
     public string firstName;
@@ -20,5 +29,28 @@ public class Person{
         this.lastName = _lastName;
         this.firstName = _firstName;
     }
-    
+}
+
+public class PeopleContainer : IPersonContainer 
+{
+    List<Person> people;
+
+    public PeopleContainer(List<Person> _people){
+        this.people = _people;
+    }
+
+    public List<Person> SortByLastName(){
+        return this.people.OrderBy(retour => retour.lastName).ToList();
+    }
+
+    public List<Person> SortByFirstName(){
+        return this.people.OrderBy(retour => retour.firstName).ToList();
+    }
+
+}
+
+interface IPersonContainer
+{
+    List<Person> SortByLastName();
+    List<Person> SortByFirstName();
 }
